beabbc478d368167115b9707e56779abf72d7c85
daeun-han <111632494+daeun-han@users.noreply.github.com>
2024-10-07 01:16:50 +0900

    [Bronze IV] Title: 영수증, Time: 176 ms, Memory: 17892 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String args[]){
+    static Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-
-        int x = scanner.nextInt();
-        int n = scanner.nextInt();
+    public static int input(int n) {
         int sum = 0;
 
         for (int i = 1; i < n+1; i++){
@@ -14,10 +11,22 @@ public class Main{
             int b = scanner.nextInt();
             sum += a*b;
         }
+        return sum;
+    }
 
+    public static void check(int sum, int x) {
         if (sum == x)
             System.out.print("Yes");
         else
             System.out.print("No");
+    }
+
+    public static void main(String args[]){
+        int x = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        int sum = input(n);
+
+        check(sum, x);
     }
 }
