4c764304b524e63b32cc487ecdd2c6c895bbbd3d
daeun-han <111632494+daeun-han@users.noreply.github.com>
2024-10-14 20:44:50 +0900

    [Bronze V] Title: A+B - 5, Time: 116 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,17 +1,22 @@
 import java.io.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String args[]) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            int A = scanner.nextInt();
-            int B = scanner.nextInt();
-            if (A == 0 && B == 0)
-                break;
-            System.out.println(A+B);
 
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int A = Integer.parseInt(stringTokenizer.nextToken());
+            int B = Integer.parseInt(stringTokenizer.nextToken());
+
+            if (A == 0 && B == 0) {
+                break; // return 대신 break로 반복문 종료
+            }
+            bufferedWriter.write((A + B) + "\n"); // 출력 형식 수정
         }
+        bufferedWriter.flush();
     }
 }
