bc1a7941b38c3d4d654b6f990ce4711ae3b0c15f
daeun-han <111632494+daeun-han@users.noreply.github.com>
2024-11-04 22:51:13 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        bufferedWriter.write("         ,r'\"7");
+        bufferedWriter.write("\n");
+        bufferedWriter.write("r`-_   ,'  ,/");
+        bufferedWriter.write("\n");
+        bufferedWriter.write(" \\. \". L_r'");
+        bufferedWriter.write("\n");
+        bufferedWriter.write("   `~\\/");
+        bufferedWriter.write("\n");
+        bufferedWriter.write("      |");
+        bufferedWriter.write("\n");
+        bufferedWriter.write("      |");
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
