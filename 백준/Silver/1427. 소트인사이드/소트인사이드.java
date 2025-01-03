74349dfcf8c280cedc76fc11442f48d5aef1041e
daeun-han <111632494+daeun-han@users.noreply.github.com>
2025-01-04 00:04:12 +0900

    [Silver V] Title: 소트인사이드, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        String[] list = new String[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            list[i] = Character.toString(arr[i]);
+        }
+        Arrays.sort(list, Collections.reverseOrder());
+        for(int i=0; i<arr.length; i++){
+            System.out.print(list[i]);
+        }
+        br.close();
+    }
+}
