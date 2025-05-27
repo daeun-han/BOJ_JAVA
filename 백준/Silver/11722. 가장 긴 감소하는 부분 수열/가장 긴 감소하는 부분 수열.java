c22323adfcf4eb4174c090bf5daa108a350f533a
daeun-han <111632494+daeun-han@users.noreply.github.com>
2025-05-27 19:18:48 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1; // 최소한 자기 자신 1개는 있으니까 길이 1로 시작
+        }
+
+        int max = 0;
+
+        // i번째 수를 마지막으로 하는 감소 부분 수열
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
